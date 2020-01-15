cd508eb6948ad1bee43c67d1bca4a914583ee55f
Rafi Rafsan <30943851+rafir123@users.noreply.github.com>
2020-01-15 00:24:22 -0500

    Add files via upload

---
+++ public/templates/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Book Seller</title>
+
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+</head>
+<body>
+	<div id="header" style="background-color:#FFA500;font-size:30px">
+		<h1 class="text" style=" text-align:center;">Online Book Seller</h1>
+	</div>
